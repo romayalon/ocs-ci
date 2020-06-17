b37a1fba2740ac3567d91ca9e973faa00e09c307
Shay Rozen <shay.rozen@gmail.com>
2020-06-17 11:26:05 +0300

    Restoring files
    Signed-off-by: Shay Rozen <shay.rozen@gmail.com>

--- ocs_ci/ocs/exceptions.py
+++ ocs_ci/ocs/exceptions.py
@@ -180,3 +180,11 @@ class RDMDiskNotFound(Exception):
 
 class CredReqSecretNotFound(Exception):
     pass
+
+
+class ClusterUtilizationNotBalanced(Exception):
+    pass
+
+
+class OsdIsIncreasing(Exception):
+    pass
